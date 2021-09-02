0dab2fde4c2d6bdcab727dd121e76d5bb779b1b0
Denis Bogoslovskiy <bogoslovskiydenis@gmail.com>
2021-09-02 22:11:20 +0300

    Add api productsSubCategories and Categories

--- ShoppBackend/Controllers/CategoriesController.cs
+++ ShoppBackend/Controllers/CategoriesController.cs
@@ -24,7 +24,7 @@ namespace ShoppBackend.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
         {
-            return await _context.Categories.ToListAsync();
+            return await _context.Categories.Include(c=> c.SubCategories).ToListAsync();
         }
 
         // GET: api/Categories/5
